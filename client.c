371ce3525845e26ca071bdc2349b1cdfa1a966fb
kyj <kyj0609@knu.ac.kr>
2019-11-20 23:07:46 +0900

    client_func

--- client.c
+++ client.c
@@ -7,19 +7,13 @@
 #include <sys/socket.h>
 #include <fcntl.h>
 #define BUFSIZE 1024
-
+char buf[BUFSIZE];
+int client_send(int, int);
 void error_handling(char* message);
+
 int main(int argc, char** argv){
-	int fd_sock;
-	int fd_source;
-	char file_buf[BUFSIZE];
-	int size = sizeof(file_buf);
-	char file_name[BUFSIZE];
-	
-	int readnum;
+	int fd_socket, fd_file, result;
 	struct sockaddr_in serv_addr;
-	//char message[BUFSIZE];
-	int str_len;
 
 	if (argc != 3) {
 		printf("Usage : %s <IP> <port> \n", argv[0]);
@@ -27,43 +21,47 @@ int main(int argc, char** argv){
 	}
 
 	// socket() 
-	fd_sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (fd_sock == -1)
+	fd_socket = socket(PF_INET, SOCK_STREAM, 0);
+	if (fd_socket == -1)
 		error_handling("socket() error");
 	memset(&serv_addr, 0, sizeof(serv_addr));
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
 	serv_addr.sin_port = htons(atoi(argv[2]));
-
 	// connect()
-	if (connect(fd_sock, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) == -1)
+	if (connect(fd_socket, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("connect() error");
-	while (1) {
-		printf("file name :");
-		scanf("%s", file_name);
-		printf(" --> %s\n", file_name);
-		//memset(file_name, 0x00, BUFSIZE);
 
-		if((fd_source = open(file_name, O_RDONLY)) == -1){
-			perror("open");
-		}
-		send(fd_sock, file_name,strlen(file_name), 0);
-		//memset(file_name, 0x00, BUFSIZE);
-		while((readnum = read(fd_source, file_buf, BUFSIZE)) > 0){
-			if(write(fd_sock, file_buf, readnum) != readnum){
-				printf("<<write?>>\n");
-				perror("write");
-				//break;
-			}
-		}
-		printf("chk");
-		if(readnum == -1){
-			perror("read");
-		}
-		close(fd_source);	
+	while (1) {
+		result = client_send(fd_file, fd_socket);
 	}
-	close(fd_sock);
+	close(fd_socket);
+	return 0;
+}
+
+int client_send(int fd_file, int fd_socket){
+	int readnum;
+	//memset(buf, 0x00, BUFSIZE);
+	printf("file name :");
+	scanf("%s", buf);
+	printf("sending ...  : %s\n", buf);
+	if((fd_file = open(buf, O_RDONLY)) == -1){
+		perror("open");
+	}
+	send(fd_socket, buf,strlen(buf), 0);
+	memset(buf, 0x00, BUFSIZE);
+	while((readnum = read(fd_file, buf, BUFSIZE)) > 0){
+		if(write(fd_socket, buf, readnum) != readnum){
+			perror("write");
+			break;
+		}
+	}
+	if(readnum == -1){
+		perror("read");
+		return -1;
+	}
+	close(fd_file);
 	return 0;
 }
 void error_handling(char* message){
